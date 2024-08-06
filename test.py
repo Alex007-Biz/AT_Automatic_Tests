0fc430d97639eb16c80d6d7d0e76905c11edf6ec
Alex007-Biz <bizukin@yandex.ru>
2024-08-06 19:18:31 +0300

    2

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
 from main import add, subtract, multiply, divide
 
-class TestMath(unittest.Testcase):
+class TestMath(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 5), 7)
         self.assertEqual(add(3, 7), 9)
