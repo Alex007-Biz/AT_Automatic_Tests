fac8e5dd26ea589b10534a48abd47fea924afc24
Alex007-Biz <bizukin@yandex.ru>
2024-08-08 20:58:19 +0300

    5

--- AT02/main5.py
+++ AT02/main5.py
@@ -4,7 +4,7 @@ def init_db():
     conn = sqlite3.connect(':memory:')
     cursor = conn.cursor()
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS user (
+    CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     name TEXT,
     age INTEGER)
--- AT02/test5.py
+++ AT02/test5.py
@@ -6,3 +6,8 @@ def db_conn():
     conn = init_db()
     yield conn
     conn.close()
+
+def test_add_or_get_user(db_conn):
+    add_user(db_conn, "Sasha", 30)
+    user = get_user(db_conn, "Sasha")
+    assert user == (1, "Sasha", 30)
