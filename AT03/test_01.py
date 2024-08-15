13b69ce49254b97b3ce7a1f75af3f7156a4d2f4b
Alex007-Biz <bizukin@yandex.ru>
2024-08-15 20:34:40 +0300

    01

--- AT03/test_01.py
+++ AT03/test_01.py
@@ -1,6 +1,9 @@
 import pytest
 from main_01 import get_weather
 
+api_key = '837f0aa0103469641de4270ffb811a57'
+city = 'London'
+
 def test_get_weather(mocker):
     mock_get = mocker.patch('main_01.requests.get')
     mock_get.return_value.status_code = 200
@@ -9,12 +12,16 @@ def test_get_weather(mocker):
         'main' : {'temp': 273.15}
     }
 
-    api_key = '837f0aa0103469641de4270ffb811a57'
-    city = 'London'
-
     weather_data = get_weather(api_key, city)
 
     assert weather_data == {
         'weather' : [{'description': 'clear sky'}],
         'main' : {'temp': 273.15}
     }
+
+def test_get_weather_with_error(mocker):
+    mock_get = mocker.patch('main_01.requests.get')
+    mock_get.return_value.status_code = 404
+    weather_data = get_weather(api_key, city)
+
+    assert weather_data == None
